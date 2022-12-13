4867d7bcbe173096b74cd5beec5bc5f032ea1804
Imbert-Dam <imbert@casema.nl>
2022-12-13 14:21:32 +0100

    Niet werkend

--- Practicum_1/Program.cs
+++ Practicum_1/Program.cs
@@ -9,7 +9,6 @@ namespace Practicum_1
     {
         static void Main(string[] args)
         {
-            int stopval = 10;
             int S_parm = 5;
             string sudoku_file = "Sudoku_puzzels_5.txt";
             string[] lines = File.ReadAllLines(sudoku_file);
@@ -20,53 +19,10 @@ namespace Practicum_1
                 {
                     stripped_line = lines[i].Remove(0,1);
                 }
-                Stopwatch watch = Stopwatch.StartNew();
                 Sudoku sud = new Sudoku(stripped_line);
-                Solver solv = new Solver(sud);
-                solv.fillWithRandom();
-                //s.printBoard();
-                solv.getBoardScore();
-                //Console.WriteLine("Score: " + s.currentScore);
-                int fails = 0;
-                int itterations = 0;
-                /*while(s.currentScore!=0)
-                {
-                    if(itterations>100000)
-                    {
-                        break;
-                    }
-                    if (fails == stopval)
-                    {
-                        fails = 0;
-                        for (int j = 0; j<S_parm; j++)
-                        {
-                            s.RandomWalkStep();
-                        }
-                        s.updateBoardScore();
-                    }
-                    else
-                    {
-                        int value = s.HillClimbStep2();
-                        if(value == -1)
-                        {
-                            fails+=1;
-                        }
-                        else{fails=0;}
-
-                    }
-                    itterations++;
-                }*/
-                int value =0;
-                while(value!=-1){
-                value = solv.HillClimbStep();
-                solv.getBoardScore();
-                Console.WriteLine(solv.s.currentScore);
-                Console.WriteLine(value);
-                }
-                watch.Stop();
-                Console.WriteLine("Spend " + watch.ElapsedMilliseconds + " Milliseconds");
-                Console.WriteLine("Score: " + solv.s.currentScore);
-                sud.printBoard();
+                Solver solv = new Solver(sud , S_parm);
+                solv.iteratedLocalSearch();
+                //sud.printBoard();
                 
                 
 
--- Practicum_1/Solver.cs
+++ Practicum_1/Solver.cs
@@ -1,17 +1,92 @@
+using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Diagnostics;
 namespace Practicum_1
 {
     public class Solver
     {
         public Sudoku s;
         Random r;
+        int parm;
 
-        public Solver(Sudoku sudoku)
+        public Solver(Sudoku sudoku , int parameter)
         {
             s = sudoku;
             r = new Random();
+            parm = parameter;
         }
 
-        public void fillWithRandom()
+        public void iteratedLocalSearch()
+        {
+            Stopwatch watch = Stopwatch.StartNew(); 
+            fillWithRandom();
+            getBoardScore();
+            int vorige_res = s.currentScore;
+            int counter = 0;
+            while (s.currentScore!=0)
+            {
+                if (counter>1000)
+                {
+                    walking();
+                }
+                HillClimbing();
+                if (s.currentScore == vorige_res)
+                {
+                    counter++;
+                    //s.printBoard();
+                }
+                else{
+                    counter = 0;
+                    vorige_res = s.currentScore;
+                }
+            }
+            watch.Stop();
+            Console.WriteLine("Spend " + watch.ElapsedMilliseconds + " Milliseconds");
+            Console.WriteLine("Score: " + s.currentScore);
+            getBoardScore();
+            Console.WriteLine("Echte Score: " + s.currentScore);
+
+
+        }
+        private void HillClimbing()
+        {
+            int res = -1;
+            List<(int,int)> boxxes = new List<(int,int)>{
+                (0,0),(0,1),(0,2),(1,0),(1,1),(1,2),(2,0),(2,1),(2,2)
+            };
+            while (res == -1)
+            {
+                if (boxxes.Count!=0)
+                {
+                    int box_idx = r.Next(boxxes.Count);
+                    int qX = boxxes[box_idx].Item1; //random box_x
+                    int qY = boxxes[box_idx].Item2; //random box_y
+                    boxxes.RemoveAt(box_idx);
+                    res = HillClimbStep2(qX,qY);         
+                }
+                else
+                {
+                    walking();
+                    //Console.WriteLine("min min");
+                    break;
+                }
+
+
+            }
+            
+        }
+        private void walking()
+        {
+            for (int j = 0; j<parm; j++)
+            {
+                RandomWalkStep();
+            }
+            getBoardScore(); //misschien dit verwerken in de swap???
+
+        }
+
+        private void fillWithRandom()
         {
             for (int qY = 0; qY < 3; qY++)
             {
@@ -133,13 +208,15 @@ namespace Practicum_1
             int x2 = r.Next(3);
             int y2 = r.Next(3);
 
+            //misschien ook hier de score updaten zonder alles langs te gaan
             Swap(qX * 3 + x1, qY * 3 + y1, qX * 3 + x2, qY * 3 + y2);
         }
 
-        public int HillClimbStep()
+        public int HillClimbStep(int qX,int qY)
         {
-            int qX = r.Next(3); //random box_x
-            int qY = r.Next(3); //random box_y
+
+            // int qX = r.Next(3); //random box_x
+            // int qY = r.Next(3); //random box_y
 
             //Console.WriteLine((qX, qY));
 
@@ -235,95 +312,106 @@ namespace Practicum_1
 
             Swap(qX * 3 + x1r, qY * 3 + y1r, qX * 3 + x2r, qY * 3 + y2r);
             s.currentScore-=maxScore;
+            int t = s.currentScore;
+
+            getBoardScore();
+            if(t!=-1 && t != s.currentScore)
+            {
+                Console.WriteLine(t);                
+                Console.WriteLine("Echte Score: " + s.currentScore);
+            }
             return s.currentScore;
         }
 
-        // public int HillClimbStep2()
-        // {
-        //     int qX = r.Next(3); //random box_x
-        //     int qY = r.Next(3); //random box_y
-        //     int maxScore = 9;
-        //     List<(int, int, int, int)> bestMoves = new List<(int, int, int, int)>();
-        //     for (int y1 = 0; y1 < 3; y1++)
-        //     {
-        //         for (int x1 = 0; x1 < 3; x1++)
-        //         {
-        //             int currentVal = board[qY * 3 + y1, qX * 3 + x1];
-        //             HashSet<int> col1 = valsInColumn(qX*3+x1, qY * 3 + y1);
-        //             HashSet<int> row1 = valsInRow(qY * 3 + y1, qX * 3 + x1);
-        //             bool c1_old = col1.Contains(currentVal);
-        //             bool r1_old = row1.Contains(currentVal);
-        //             for (int y2 = y1; y2 < 3; y2++)
-        //             {
-        //                 for (int x2 = x1; x2 < 3; x2++)
-        //                 {
-        //                     /* loop alle mogelijke vakjes combinaties */
-        //                     if ((x1 != x2 || y1 != y2)&&(!unmovable[qY * 3 + y1, qX * 3 + x1])&&(!unmovable[qY * 3 + y2, qX * 3 + x2])) //punt x1,y1 != punt x2,y2
-        //                     {
-        //                         int tempVal = board[qY * 3 + y2, qX * 3 + x2];
-        //                         int col_score = 0;
-        //                         int row_score = 0;
-        //                         if(!(x1==x2))
-        //                         {
-        //                             HashSet<int> col2 = valsInColumn(qX*3+x2, qY * 3 + y2);
+        public int HillClimbStep2( int qX , int qY)
+        {
+            int maxScore = 0;
+            List<(int, int, int, int)> bestMoves = new List<(int, int, int, int)>();
+            for (int y1 = 0; y1 < 3; y1++)
+            {
+                for (int x1 = 0; x1 < 3; x1++)
+                {
+                    int currentVal = s.board[qY * 3 + y1, qX * 3 + x1];
+                    HashSet<int> col1 = valsInColumn(qX*3+x1, qY * 3 + y1);
+                    HashSet<int> row1 = valsInRow(qY * 3 + y1, qX * 3 + x1);
+                    bool c1_old = col1.Contains(currentVal);
+                    bool r1_old = row1.Contains(currentVal);
+                    for (int y2 = 0; y2 < 3; y2++)
+                    {
+                        for (int x2 = 0; x2 < 3; x2++)
+                        {
+                            /* loop alle mogelijke vakjes combinaties */
+                            if ((x1 != x2 || y1 != y2)&&(!s.unmovable[qY * 3 + y1, qX * 3 + x1])&&(!s.unmovable[qY * 3 + y2, qX * 3 + x2])) //punt x1,y1 != punt x2,y2
+                            {
+                                int tempVal = s.board[qY * 3 + y2, qX * 3 + x2];
+                                int col_score = 0;
+                                int row_score = 0;
+                                if(!(x1==x2))
+                                {
+                                    HashSet<int> col2 = valsInColumn(qX*3+x2, qY * 3 + y2);
                                     
-        //                             bool c1_new = col1.Contains(tempVal);
-        //                             bool c2_old = col2.Contains(tempVal);
-        //                             bool c2_new = col2.Contains(currentVal);
-        //                             col_score+=scorecheck(c1_old,c1_new);
-        //                             col_score+=scorecheck(c2_old,c2_new);
-        //                         }
-        //                         if(!(y1==y2))
-        //                         {
-        //                             HashSet<int> row2 = valsInRow(qY * 3 + y2, qX * 3 + x2);
-        //                             bool r1_new = row1.Contains(tempVal);
-        //                             bool r2_old = row2.Contains(tempVal);
-        //                             bool r2_new = row2.Contains(currentVal); //kan efficienter geplaatst worden
-        //                             row_score+=scorecheck(r1_old,r1_new);
-        //                             row_score+=scorecheck(r2_old,r2_new);
-        //                         }
-        //                         int score = col_score+row_score;
-        //                         if(score<maxScore)
-        //                         {
-        //                             bestMoves = new List<(int, int, int, int)>();
-        //                             maxScore = score;
-        //                             bestMoves.Add((x1, y1, x2, y2));
-        //                         }
-        //                         else if (score == maxScore)
-        //                         {
-        //                             bestMoves.Add((x1, y1, x2, y2));
-        //                         }
-        //                     }
-        //                 }
-        //             }
-        //         }
-        //     }
-        //     if (bestMoves.Count == 0)
-        //     {
-        //         return -1;
-        //     }
-        //     int temp = r.Next(bestMoves.Count);
+                                    bool c1_new = col1.Contains(tempVal);
+                                    bool c2_old = col2.Contains(tempVal);
+                                    bool c2_new = col2.Contains(currentVal);
+                                    col_score+=scorecheck(c1_old,c1_new);
+                                    col_score+=scorecheck(c2_old,c2_new);
+                                }
+                                if(!(y1==y2))
+                                {
+                                    HashSet<int> row2 = valsInRow(qY * 3 + y2, qX * 3 + x2);
+                                    bool r1_new = row1.Contains(tempVal);
+                                    bool r2_old = row2.Contains(tempVal);
+                                    bool r2_new = row2.Contains(currentVal); //kan efficienter geplaatst worden
+                                    row_score+=scorecheck(r1_old,r1_new);
+                                    row_score+=scorecheck(r2_old,r2_new);
+                                }
+                                int score = col_score+row_score;
+                                if(score<maxScore)
+                                {
+                                    bestMoves = new List<(int, int, int, int)>();
+                                    maxScore = score;
+                                    bestMoves.Add((x1, y1, x2, y2));
+                                }
+                                else if (score == maxScore)
+                                {
+                                    bestMoves.Add((x1, y1, x2, y2));
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            if (bestMoves.Count == 0)
+            {
+                //Console.WriteLine("min");
+                return -1;
+            }
+            int temp = r.Next(bestMoves.Count);
 
-        //     (int x1r, int y1r, int x2r, int y2r) = bestMoves[temp];
+            (int x1r, int y1r, int x2r, int y2r) = bestMoves[temp];
 
-        //     Swap(qX * 3 + x1r, qY * 3 + y1r, qX * 3 + x2r, qY * 3 + y2r);
+            Swap(qX * 3 + x1r, qY * 3 + y1r, qX * 3 + x2r, qY * 3 + y2r);
 
-        //     currentScore+=maxScore;
-        //     return currentScore;
-        // }
+            s.currentScore+=maxScore;
+            //Console.WriteLine(s.currentScore);
+            //getBoardScore();
+            //Console.WriteLine(s.currentScore);
+            return s.currentScore;
+        }
 
-        // private int scorecheck(bool old, bool nieuw)
-        // {
-        //     if (old && !nieuw)
-        //     {
-        //         return -1;
-        //     }
-        //     else if ((old && nieuw)||!old && !nieuw)
-        //     {
-        //         return 0;
-        //     }
-        //     return 1;
-        // }
+        private int scorecheck(bool old, bool nieuw)
+        {
+            if (old && !nieuw)
+            {
+                return -1;
+            }
+            else if ((old && nieuw)||!old && !nieuw)
+            {
+                return 0;
+            }
+            return 1;
+        }
+
 
 
 
