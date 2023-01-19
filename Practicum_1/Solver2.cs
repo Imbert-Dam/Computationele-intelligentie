b74de86f84ffcbb0f052852c6a630bb6d913cb57
Imbert-Dam <imbert@casema.nl>
2023-01-19 18:39:27 +0100

    Structure changes

--- Practicum_1/Solver2.cs
+++ Practicum_1/Solver2.cs
@@ -10,12 +10,18 @@ namespace Practicum_1
         private Stack<(int,int,int,int)> st = new Stack<(int, int, int, int)>();
         private int counter = 0;
         public Solver2(Sudoku sudoku)
-        {
+        {/*
+        Constructor requires a:
+            - sudoku object
+        */
             s=sudoku;
         }
 
         public long ChronologicalBacktracking()
-        {
+        {/*
+        ChronologicalBacktracking() solves a sudoku using chronological backtracking and returns 
+            the amount of ticks it took.
+        */
             Stopwatch watch = Stopwatch.StartNew();
             //CBT
             CBT();
@@ -24,7 +30,10 @@ namespace Practicum_1
         }
 
         public long Backtracking()
-        {
+        {/*
+        Backtracking() solves a sudoku using backtracking and returns 
+            the amount of ticks it took.
+        */
             Stopwatch watch = Stopwatch.StartNew();
             //CBT
             BT();
@@ -33,10 +42,14 @@ namespace Practicum_1
         }
 
         private bool BT()
-        {
+        {/*
+        BT() gets the next empty square and fills in a number ranging from 1 up untill 9 if it is
+            legal in the same: row , column and box 
+            through recursion it checks if this number is possible, if not it raises the number.
+        */
             (int x,int y) = NextSquare(); 
             if (x == -1)
-            { //base case
+            { // base case: no square is empty
                 return true;
             }              
             for (int i=1; i<10; i++)
@@ -48,7 +61,7 @@ namespace Practicum_1
                     {
                         return true;
                     }
-                    //nu is ie incorrect:
+                    //if this line is reached there was an incorrect number filled in
                     s.board[y,x] = 0;
                 }
             }
@@ -56,10 +69,15 @@ namespace Practicum_1
         }
 
         private bool CBT()
-        {
+        {/*
+        BT() gets the next empty square and fills in a number ranging from 1 up untill 9 if it is
+            in the domain
+            through recursion and forward checking it checks if this number is possible, 
+            if not it returns all elements of the domain and it raises the number.
+        */
             (int cx,int cy) = NextSquare();
             if (cx == -1)
-            { //base case
+            { // base case: no square is empty
                 return true;
             }
 
@@ -70,68 +88,75 @@ namespace Practicum_1
                 {
                     s.board[cy,cx] = i;
                     st.Push((counter,i,cy,cx));
-                    bool empty = false;
-                    for(int j = 0; j<9;j++)
-                    {
-                        int by = cy/3;
-                        int bx = cx/3;
-                        int y = by*3 + j/3;
-                        int x = bx*3 + j%3; // functie maken in sudoku class gezien 2x aangeroepen
-
-                        
-                        if(!(cy==j)&&s.domain[j,cx].Remove(i))
-                        {
-                            st.Push((counter,i,j,cx));
-                        }
-                        if(s.domain[j,cx].Count == 0 && s.board[j,cx] == 0)
-                        {
-                            empty = true;
-                            break;
-                        }   
-                        if(!(cx==j)&&s.domain[cy,j].Remove(i))
-                        {
-                            st.Push((counter,i,cy,j));
-                        }
-                        if(s.domain[cy,j].Count == 0 && s.board[cy,j] == 0)
-                        {
-                            empty = true;
-                            break;
-                        }  
-                        if(!((cy==y)&&(cx==x))&&s.domain[y,x].Remove(i))
-                        {
-                            st.Push((counter,i,y,x));
-                        }
-                        if(s.domain[y,x].Count == 0 && s.board[y,x] == 0)
-                        {
-                            empty =true;
-                            break;
-                        }
-
-                    }
-                    counter++;
-                    if(!empty&&CBT())
+                    if(!forwardCheck(i,cy,cx)&&CBT())
                     {
                         return true;
                     }
                     counter--;
                     s.board[cy,cx] = 0;
-                    if(st.Count!=0)
+                    while(st.Count!=0 && st.Peek().Item1 == counter)
                     {
-                        
-                        while(st.Count!=0 && st.Peek().Item1 == counter)
-                        {
-                            (int t,int v,int c_y,int c_x)= st.Pop();
-                            s.domain[c_y,c_x].Add(v);
-                        }
-                    } 
+                        (int t,int v,int c_y,int c_x)= st.Pop();
+                        s.domain[c_y,c_x].Add(v);
+                    }
+                    
                 }
             }
             return false;
 
         }
+        private bool forwardCheck(int i , int cy, int cx)
+        { /*
+        Removes i from all the domains that square cy,cx has effect on if is a different square.
+        Returns false if it is still a partial solution.
+        Returns true if a domain of a empty square is empty.
+        */
+            for(int j = 0; j<9;j++)
+            {
+                int by = cy/3;
+                int bx = cx/3;
+                int y = by*3 + j/3;
+                int x = bx*3 + j%3; // functie maken in sudoku class gezien 2x aangeroepen
+
+                        
+                if(!(cy==j)&&s.domain[j,cx].Remove(i))
+                {
+                    st.Push((counter,i,j,cx));
+                }
+                if(s.domain[j,cx].Count == 0 && s.board[j,cx] == 0)
+                {
+                    counter++;
+                    return true;
+                }   
+                if(!(cx==j)&&s.domain[cy,j].Remove(i))
+                {
+                    st.Push((counter,i,cy,j));
+                }
+                if(s.domain[cy,j].Count == 0 && s.board[cy,j] == 0)
+                {
+                    counter++;
+                    return true;
+                }  
+                if(!((cy==y)&&(cx==x))&&s.domain[y,x].Remove(i))
+                {
+                    st.Push((counter,i,y,x));
+                }
+                if(s.domain[y,x].Count == 0 && s.board[y,x] == 0)
+                {
+                    counter++;
+                    return true;
+                }
+
+            }
+            counter++;
+            return false;
+            
+        }
 
         private (int,int) NextSquare()
-        {
+        {/*
+        NextSquare() returns the next empty square if the sudoku is finished (-1,-1) is returned.
+        */
             for (int y = 0; y<9; y++)
             {
                 for (int x = 0; x<9; x++)
