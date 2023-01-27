3777c442c1f93bb9a9f23c6ef4bdd1faea41348f
Imbert-Dam <imbert@casema.nl>
2023-01-27 17:23:34 +0100

    xtra experiment plus comments

--- Practicum_2/Program.cs
+++ Practicum_2/Program.cs
@@ -19,9 +19,10 @@ namespace Practicum_1
             using (StreamWriter sw = new StreamWriter("Results.txt"))
                 {
                     /*Following lines are used for formatting the Result.txt file*/
-                        sw.WriteLine("Optimized: Unoptimized:");
-                        //sw.WriteLine("CBT: BT:");
+                        //sw.WriteLine("Optimized: Unoptimized:");
+                        //sw.WriteLine("CBT: BT: CBTbacktracks: BTbacktracks:");
                         //sw.WriteLine("CBT: ILS:");
+                        sw.WriteLine("BT: CBT: ILS:");
                     for (int i = 1; i<lines.Length;i+=2) //ignore grid lines
                     { 
                         string stripped_line = lines[i];
@@ -31,7 +32,7 @@ namespace Practicum_1
                         }
                         /* The following line tests the speed difference between the optimized
                                 and unoptimized version: */
-                        SpeedTest(stripped_line,sw);
+                        //SpeedTest(stripped_line,sw);
                         
                         /* The following line tests the speed difference between backtracking
                                 and chronological backtracking */
@@ -40,6 +41,10 @@ namespace Practicum_1
                         /* The following line tests the speed difference between iterated local search
                                 and chronological backtracking */
                         //CBTvsILS(stripped_line,sw);
+
+                        /* The following line tests the speed difference between iterated local search, backtracking 
+                                and chronological backtracking */
+                        BTvsCBTvsILS(stripped_line,sw);
 
                         /* The following lines solves sudokus with the best version: */
                         Sudoku sud = new Sudoku(stripped_line,"CBT",true);
@@ -66,7 +71,7 @@ namespace Practicum_1
         }
 
         static void BTvsCBT(string s, StreamWriter sw)
-        {/*SpeedTest tests the speed of the backtracking and chronologicalbacktracking */
+        {/*BTvsCBT tests the speed of the backtracking and chronologicalbacktracking */
 
             //Test with Chronological Backtracking:
             Sudoku sud = new Sudoku(s,"CBT",true);
@@ -82,9 +87,7 @@ namespace Practicum_1
         }          
 
         static void CBTvsILS(string s, StreamWriter sw)
-        { /*SpeedTest tests the speed of the unoptimized and optimized versions of the
-                chronological backtracking, it is done by solving the same sudoku n times and
-                then taking the average.*/
+        { /*CBTvsILS tests the speed of the CBT and ILS solvers. */
             long ils = 0;
             for (int i = 0; i<Max_n; i++)
             {
@@ -104,6 +107,38 @@ namespace Practicum_1
             w2.Stop();
             long cbt = w2.ElapsedTicks;
             sw.WriteLine($"{cbt} {ils/Max_n}");
+        }
+
+        static void BTvsCBTvsILS(string s, StreamWriter sw)
+        {/*BTvsCBTvsILS tests the speed of the backtracking, chronologicalbacktracking and ILS */
+            long ils = 0;
+            for (int i = 0; i<Max_n; i++)
+            { //since ILS is non-deterministic average of 50 times
+                //Test with ILS
+                Stopwatch w1 = Stopwatch.StartNew();
+                Sudoku sudo = new Sudoku(s);
+                Solver so = new Solver(sudo,S_parm,P_parm);
+                so.iteratedLocalSearchOptimized();
+                w1.Stop();
+                ils+=w1.ElapsedTicks;
+            }
+            //Test with ChronologicalBacktracking:
+            Stopwatch w2 = Stopwatch.StartNew();
+            Sudoku su = new Sudoku(s,"CBT",true);
+            Solver2 sol = new Solver2(su);
+            sol.ChronologicalBacktrackingOptimized();
+            w2.Stop();
+            long cbt = w2.ElapsedTicks;
+
+            //Test with Backtracking:
+            Stopwatch w3 = Stopwatch.StartNew();
+            Sudoku sudok = new Sudoku(s);
+            Solver2 solv = new Solver2(sudok);
+            sol.BacktrackingOptimized();
+            w3.Stop();
+            long bt = w3.ElapsedTicks;
+            sw.WriteLine($"{bt} {cbt} {ils/Max_n}");
+
         }
         
     }
